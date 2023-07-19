34d8763c869a5e9da5d1c864f2473f336b8b8aa3
Karan Singh <ksingh2826@gmail.com>
2023-07-19 17:10:40 +0530

    biggest contour and reorder points functions added

--- utils.py
+++ utils.py
@@ -9,6 +9,31 @@ def preProcess(img):
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, 1, 1, 11, 2)  # Add Adaptive Threshold
     return imgThreshold
 
+#### 3. FINDING THE BIGGEST CONTOUR ENSURING ITS A SUDOKU PUZZLE
+def biggestContour(contours):
+    biggest = np.array([])  # WILL CONTAIN ALL THE CORNER POINTS, NOT NECESSARILY ORDERED
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)  # FIND PERIMETER
+            approx = cv2.approxPolyDP(i, 0.02*peri,True)  # FIND THE NUMBER OF CORNERS
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest, max_area
+
+#### 3. REORDER POINTS FOR WARP PERSPECTIVE
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointsNew = np.zeros((4,1,2), dtype=np.int32)
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
 
 #### 6 - TO STACK ALL THE IMAGES IN ONE WINDOW
 def stackImages(imgArray,scale):
